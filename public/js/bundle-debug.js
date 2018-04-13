b42b4cc00f5f1a467f40c486b81b3e12ef7f34d9
Tran Hieu <snrikikudo@gmail.com>
2018-04-13 22:29:27 +0700

    first commit

---
+++ public/js/bundle-debug.js
@@ -0,0 +1,46 @@
+jQuery(document).ready(function(){
+  var owl = $('.slider__auto .owl-carousel');
+  owl.each(function(index) {
+    $(this).owlCarousel({
+      items:5,
+      loop:true,
+      margin:10,
+      autoplay:true,
+      autoplayTimeout:3000,
+      autoplayHoverPause:true,
+      responsiveClass:true,
+      responsive:{
+          0:{
+              items:2,
+              nav:true
+          },
+          600:{
+              items:3,
+              nav:false
+          },
+          1000:{
+              items:5,
+              nav:true,
+              loop:false
+          }
+      }
+  });
+  })
+  
+  $('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+});
